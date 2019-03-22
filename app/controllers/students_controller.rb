4efc1151e727f1ec37fdf83e00e67a177ff51dd8
grant smith <grantsmith1256@gmail.com>
2019-03-22 00:28:38 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
 
   def index
-    index
+    @students = Student.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
 
   get "/students", to: "students#index"
 
-  resources :students, only: [:index]
 end
